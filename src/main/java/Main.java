07bd142cc893f3a31c4c580d3c51163b31cd0b28
Souukou <i@syh.one>
2022-11-20 20:40:29 -0500

    Add maven development framework (#2)

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import legendsofvalor.GamePlatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GamePlatform game = new GamePlatform();
+        game.startGame();
+    }
+}
---
+++ src/test/java/legendsofvalor/TestGamePlatform.java
@@ -0,0 +1,16 @@
+package legendsofvalor;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+public class TestGamePlatform {
+
+    @Test
+    public void TestGamePlatformCreation() {
+        GamePlatform game = new GamePlatform();
+        game.startGame();
+        Assertions.assertTrue(true);
+        Assertions.assertEquals("Test", "Test"); // Write unit tests like this
+    }
+}
