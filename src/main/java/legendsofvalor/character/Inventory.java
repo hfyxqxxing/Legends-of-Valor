8c1470ce7a0da7156ad3a8b2add84b5b6014dab2
Yuhang Song <i@syh.one>
2022-11-30 05:26:35 -0500

    add potion view and inventary

--- src/main/java/legendsofvalor/character/Inventory.java
+++ src/main/java/legendsofvalor/character/Inventory.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 import legendsofvalor.item.*;
+import legendsofvalor.utils.GameUtils;
 
 class SortbyItemType implements Comparator<InventoryItem> {
     public int compare(InventoryItem a, InventoryItem b) {
@@ -54,14 +55,44 @@ public class Inventory extends CharacterAttribute {
         Collections.sort(items, new SortbyItemType());
     }
 
-    public ArrayList<Potion> getAllPotions() {
-        ArrayList<Potion> potions = new ArrayList<Potion>();
+    public ArrayList<Potion> getAllPotion() {
+        ArrayList<Potion> potionList = new ArrayList<Potion>();
         for (InventoryItem item : items) {
             if (item.getItemType() == "Potion") {
-                potions.add((Potion) item);
+                potionList.add((Potion) item);
             }
         }
-        return potions;
+        return potionList;
+    }
+
+    public ArrayList<Spell> getAllSpell() {
+        ArrayList<Spell> spellList = new ArrayList<Spell>();
+        for (InventoryItem item : items) {
+            if (item.getItemType() == "Spell") {
+                spellList.add((Spell) item);
+            }
+        }
+        return spellList;
+    }
+
+    public ArrayList<Weapon> getAllWeapon() {
+        ArrayList<Weapon> weaponList = new ArrayList<Weapon>();
+        for (InventoryItem item : items) {
+            if (item.getItemType() == "Weapon") {
+                weaponList.add((Weapon) item);
+            }
+        }
+        return weaponList;
+    }
+
+    public ArrayList<Armor> getAllArmor() {
+        ArrayList<Armor> armorList = new ArrayList<Armor>();
+        for (InventoryItem item : items) {
+            if (item.getItemType() == "Armor") {
+                armorList.add((Armor) item);
+            }
+        }
+        return armorList;
     }
 
     @Override
@@ -92,80 +123,57 @@ public class Inventory extends CharacterAttribute {
 
     }
 
-    public String getWeaponString() {
-        this.sortItems();
-        String re = "";
-        for (int i = 0; i < items.size(); ++i) {
-            if (items.get(i).getItemType().equals("Weapon")) {
-                Weapon curWeapon = (Weapon) items.get(i);
-                if (re.length() == 0) {
-                    String header = curWeapon.getWeaponHeader();
-                    re += "     " + header + "\n" + "     " + String.join("", Collections.nCopies(header.length(), "-")) + "\n";
-                }
-                re += String.format("%-5s", "[" + i + "]") + curWeapon.getWeaponBody() + "\n";
-            }
+    public String getSpellString() {
+        ArrayList<Spell> spellList = this.getAllSpell();
+        if (spellList.size() == 0) {
+            return "No spells in inventory";
         }
-        if (re.length() == 0) {
-            re = "No weapons in inventory";
+        String header = spellList.get(0).getSpellHeader();
+        ArrayList<String> spellBodyList = new ArrayList<String>();
+        for (Spell spell : spellList) {
+            spellBodyList.add(spell.getSpellBody());
         }
-        return re;
+        return GameUtils.getNiceTable(header, spellBodyList);
     }
 
-    public String getPotionString() {
-        this.sortItems();
-        String re = "";
-        for (int i = 0; i < items.size(); ++i) {
-            if (items.get(i).getItemType().equals("Potion")) {
-                Potion curPotion = (Potion) items.get(i);
-                if (re.length() == 0) {
-                    String header = curPotion.getPotionHeader();
-                    re += "     " + header + "\n" + "     " + String.join("", Collections.nCopies(header.length(), "-")) + "\n";
-                }
-                re += String.format("%-5s", "[" + i + "]") + curPotion.getPotionBody() + "\n";
-            }
+    public String getWeaponString() {
+        ArrayList<Weapon> weaponList = this.getAllWeapon();
+        if (weaponList.size() == 0) {
+            return "No weapons in inventory";
         }
-        if (re.length() == 0) {
-            re = "No potion in inventory";
+        String header = weaponList.get(0).getWeaponHeader();
+        ArrayList<String> weaponBodyList = new ArrayList<String>();
+        for (Weapon weapon : weaponList) {
+            weaponBodyList.add(weapon.getWeaponBody());
         }
-        return re;
+        return GameUtils.getNiceTable(header, weaponBodyList);
     }
 
     public String getArmorString() {
-        this.sortItems();
-        String re = "";
-        for (int i = 0; i < items.size(); ++i) {
-            if (items.get(i).getItemType().equals("Armor")) {
-                Armor curArmor = (Armor) items.get(i);
-                if (re.length() == 0) {
-                    String header = curArmor.getArmorHeader();
-                    re += "     " + header + "\n" + "     " + String.join("", Collections.nCopies(header.length(), "-")) + "\n";
-                }
-                re += String.format("%-5s", "[" + i + "]") + curArmor.getArmorBody() + "\n";
-            }
+        ArrayList<Armor> armorList = this.getAllArmor();
+        if (armorList.size() == 0) {
+            return "No armors in inventory";
         }
-        if (re.length() == 0) {
-            re = "No armor in inventory";
+        String header = armorList.get(0).getArmorHeader();
+        ArrayList<String> armorBodyList = new ArrayList<String>();
+        for (Armor armor : armorList) {
+            armorBodyList.add(armor.getArmorBody());
         }
-        return re;
+        return GameUtils.getNiceTable(header, armorBodyList);
     }
 
-    public String getSpellString() {
-        this.sortItems();
-        String re = "";
-        for (int i = 0; i < items.size(); ++i) {
-            if (items.get(i).getItemType().equals("Spell")) {
-                Spell curSpell = (Spell) items.get(i);
-                if (re.length() == 0) {
-                    String header = curSpell.getSpellHeader();
-                    re += "     " + header + "\n" + "     " + String.join("", Collections.nCopies(header.length(), "-")) + "\n";
-                }
-                re += String.format("%-5s", "[" + i + "]") + curSpell.getSpellBody() + "\n";
-            }
+    public String getPotionString() {
+        ArrayList<Potion> potionList = this.getAllPotion();
+        if (potionList.size() == 0) {
+            return "No potions in inventory";
         }
-        if (re.length() == 0) {
-            re = "No spell in inventory";
+        String header = potionList.get(0).getPotionHeader();
+        ArrayList<String> potionBodyList = new ArrayList<String>();
+        for (Potion potion : potionList) {
+            potionBodyList.add(potion.getPotionBody());
         }
-        return re;
+        return GameUtils.getNiceTable(header, potionBodyList);
     }
+
 
 }
--- src/main/java/legendsofvalor/view/PotionView.java
+++ src/main/java/legendsofvalor/view/PotionView.java
@@ -24,21 +24,19 @@ public class PotionView {
                 continue;
             }
             if (comm == 1) {
-                while (true) {
-                    ArrayList<Potion> potions = curHero.getInventory().getAllPotions();
-                    ColorPrint.query("Please select a potion to consume, or enter [0] to cancel and go back");
-                    int index = UserInput.getInstance().getChoice(0, potions.size());
-                    if (index == 0) {
-                        break;
-                    }
-                    Potion curPotion = potions.get(index - 1);
-                    curHero.getInventory().removeItem(curPotion);
-                    curPotion.effect(curHero);
-                    ColorPrint.green(
-                            "You have consumed " + curPotion.getName() + " successfully. New hero information:");
-                    System.out.println(GameUtils.getCharacterTable(curHero));
-                    break;
+                ArrayList<Potion> potionList = curHero.getInventory().getAllPotion();
+                ColorPrint.query("Please select a potion to consume, or enter [0] to cancel and go back");
+                int index = UserInput.getInstance().getChoice(0, potionList.size());
+                if (index == 0) {
+                    return false;
                 }
+                Potion curPotion = potionList.get(index - 1);
+                curHero.getInventory().removeItem(curPotion);
+                curPotion.effect(curHero);
+                ColorPrint.green(
+                        "You have consumed " + curPotion.getName() + " successfully. New hero information:");
+                System.out.println(GameUtils.getCharacterTable(curHero));
+
                 return true;
             }
         }
