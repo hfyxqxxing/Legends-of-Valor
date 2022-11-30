711cde8cd3af6130b1a73e9aa4baf151db29950c
Yuhang Song <i@syh.one>
2022-11-30 17:49:39 -0500

    fix spell view

--- src/main/java/legendsofvalor/view/SpellView.java
+++ src/main/java/legendsofvalor/view/SpellView.java
@@ -6,36 +6,41 @@ import legendsofvalor.character.Hero;
 import legendsofvalor.character.Monster;
 import legendsofvalor.item.Spell;
 import legendsofvalor.utils.ColorPrint;
+import legendsofvalor.utils.GameUtils;
 import legendsofvalor.utils.UserInput;
+import legendsofvalor.world.WorldMap;
 
 public class SpellView {
-    public static boolean view(Hero curHero, Monster monster) {
-        ColorPrint.green("Current Hero's Spell:\n");
-        ColorPrint.plain(curHero.getInventory().getSpellString());
-        while (true) {
-            ColorPrint.query(
-                    "Please enter [1] to perform spell attack, or enter [0] to cancel and go back:");
-            int comm = UserInput.getInstance().getChoice(0, 1);
-            if (comm == 0) {
-                ColorPrint.info("Cancel spell attack");
-                return false;
-            }
-            if (comm == 1) {
-                ArrayList<Spell> spellList = curHero.getInventory().getAllSpell();
-                ColorPrint.query("Please select a spell to attack, or enter [0] to cancel and go back:");
-                int index = UserInput.getInstance().getChoice(0, spellList.size());
-                if (index == 0) {
-                    return false;
-                }
-                Spell curSpell = spellList.get(index - 1);
-                if (curSpell.getManaCost() > curHero.getMP().get()) {
-                    ColorPrint.error("You don't have enough mana to perform this spell attack");
-                    continue;
-                }
-                // use the current spell
-                curSpell.attack(curHero, monster);
-                return true;
-            }
+    public static boolean view(Hero hero) {
+        ArrayList<Monster> monsters = WorldMap.getInstance().getAttackScope(hero);
+        if (monsters.size() == 0) {
+            ColorPrint.error("No monster in attack scope");
+            return false;
         }
+        ColorPrint.green("Current Hero's Spell:\n");
+        ColorPrint.plain(hero.getInventory().getSpellString());
+        ArrayList<Spell> spellList = hero.getInventory().getAllSpell();
+        ColorPrint.query("Please select a spell to attack, or enter [0] to cancel and go back:");
+        int index = UserInput.getInstance().getChoice(0, spellList.size());
+        if (index == 0) {
+            return false;
+        }
+        Spell curSpell = spellList.get(index - 1);
+        if (curSpell.getManaCost() > hero.getMP().get()) {
+            ColorPrint.error("You don't have enough mana to perform this spell attack");
+            return false;
+        }
+        ColorPrint.info("Please choose a monster to attac, or enter [0] to cancel and go back:");
+        ColorPrint.plain(GameUtils.getCharacterTable(monsters));
+        index = UserInput.getInstance().getChoice(1, monsters.size());
+        if (index == 0) {
+            return false;
+        }
+        Monster curMonster = monsters.get(index - 1);
+        curSpell.attack(hero, curMonster);
+
+        return true;
+
     }
+
 }
